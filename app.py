fe4c0efa0f5aa65f249af46303d0f63047cd4987
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-21 15:22:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from faster_whisper import WhisperModel
+import base64
 
 class InferlessPythonModel:
         
     def initialize(self):
         model_size = "large-v3"
         self.model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    
+    def base64_to_mp3(base64_data, output_file_path):
+        mp3_data = base64.b64decode(base64_data)
+        with open(output_file_path, "wb") as mp3_file:
+                mp3_file.write(mp3_data)
 
     def infer(self, inputs):
-        
-        audio_url = inputs["audio_url"]
-        segments, info = self.model.transcribe(audio_url, beam_size=5)
+        audio_data = inputs["audio_base64"]
+        audio_file = "output.mp3"
+        base64_to_mp3(audio_data,audio_file)
+        segments, info = self.model.transcribe(audio_file, beam_size=5)
         text = ''.join([segment.text for segment in segments])
         
         return {"transcribed_output":text}
